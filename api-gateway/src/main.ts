c270aff25efcf07dbb288ca0a95b0896264d411c
Fabian <fabian.bello@usach.cl>
2022-12-07 00:00:48 -0400

    Interceptors timeout
    Manejo de tiempo para respuesta por medio de interceptor

--- api-gateway/src/main.ts
+++ api-gateway/src/main.ts
@@ -1,10 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { AllExceptionFilter } from './common/filters/http-exception.filter';
+import { TimeOutInterceptor } from './common/interceptors/timeout.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // manejo de excepciones globales
   app.useGlobalFilters(new AllExceptionFilter());
+  // manejo de tiempo maximo de respuesta
+  app.useGlobalInterceptors(new TimeOutInterceptor());
   await app.listen(process.env.PORT || 3000);
   console.log('API Gateway de meetflow corriendo en el puerto: ', process.env.PORT);
 }
