1d8280e7f8c3ffbb0b508b23426c5f8cca9f5ba3
Fabian <fabian.bello@usach.cl>
2022-12-07 03:20:08 -0400

    Controller ms guest

--- microservice-guests/src/guest/guest.controller.ts
+++ microservice-guests/src/guest/guest.controller.ts
@@ -1,35 +1,36 @@
 import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import { MessagePattern, Payload } from '@nestjs/microservices';
+import { GuestMSG } from 'src/common/constants';
 import { GuestDTO } from './dto/guest.dto';
 import { GuestService } from './guest.service';
 
-@ApiTags('guests')
-@Controller('api/guest')
+
+@Controller()
 export class GuestController {
   constructor(private readonly guestService: GuestService) {}
 
-  @Post()
-  create(@Body() guestDTO: GuestDTO) {
+  @MessagePattern(GuestMSG.CREATE)
+  create(@Payload() guestDTO: GuestDTO) {
     return this.guestService.create(guestDTO);
   }
 
-  @Get()
+  @MessagePattern(GuestMSG.FIND_ALL)
   findAll(){
     return this.guestService.findAll();
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string){
+  @MessagePattern(GuestMSG.FIND_ONE)
+  findOne(@Payload() id: string){
     return this.guestService.findOne(id);
   }
 
-  @Put(':id')
-  update(@Param('id') id: string, @Body() guestDTO: GuestDTO){
-    return this.guestService.update(id, guestDTO);
+  @MessagePattern(GuestMSG.UPDATE)
+  update(@Payload() payload: any){
+    return this.guestService.update(payload.id, payload.guestDTO);
   }
 
-  @Delete(':id')
-  delete(@Param('id') id: string){
+  @MessagePattern(GuestMSG.DELETE)
+  delete(@Payload() id: string){
     return this.guestService.delete(id);
   }
   
