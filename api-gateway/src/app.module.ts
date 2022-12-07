c4cffbae3885ba7d3753fd95970efb0892bc1fff
Fabian <fabian.bello@usach.cl>
2022-12-06 23:53:18 -0400

    Config global
    se configura directorio de variables de entorno

--- api-gateway/src/app.module.ts
+++ api-gateway/src/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: ['.env.development'],
+      isGlobal: true,
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- api-gateway/src/main.ts
+++ api-gateway/src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
+  console.log('API Gateway de meetflow corriendo en el puerto: ', process.env.PORT);
 }
 bootstrap();
