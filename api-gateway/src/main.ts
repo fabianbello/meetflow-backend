c8a79c0a58e69b41954edaac0f05a9f78d8c71c3
Fabian <fabian.bello@usach.cl>
2022-12-06 23:58:28 -0400

    Filters exceptions
    configuracionde filtros para excepciones a nivel global

--- api-gateway/src/main.ts
+++ api-gateway/src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { AllExceptionFilter } from './common/filters/http-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalFilters(new AllExceptionFilter());
   await app.listen(process.env.PORT || 3000);
   console.log('API Gateway de meetflow corriendo en el puerto: ', process.env.PORT);
 }
