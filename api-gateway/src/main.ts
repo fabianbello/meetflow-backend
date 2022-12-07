d712aafae5060855258d2c8ccd0673f320f30637
Fabian <fabian.bello@usach.cl>
2022-12-07 00:03:58 -0400

    Validations
    configuracion global para validaciones

--- api-gateway/src/main.ts
+++ api-gateway/src/main.ts
@@ -1,3 +1,4 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { AllExceptionFilter } from './common/filters/http-exception.filter';
@@ -7,8 +8,11 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   // manejo de excepciones globales
   app.useGlobalFilters(new AllExceptionFilter());
-  // manejo de tiempo maximo de respuesta
+  // manejo de tiempo maximo de respuesta globales
   app.useGlobalInterceptors(new TimeOutInterceptor());
+  // manejo de validaciones a respuestas globales
+  app.useGlobalPipes(new ValidationPipe());
+
   await app.listen(process.env.PORT || 3000);
   console.log('API Gateway de meetflow corriendo en el puerto: ', process.env.PORT);
 }
