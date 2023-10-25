c7ac2fa58351579dbaa6da8fef6a3f65d8c62da1
Fabián <fabian.bello@usach.cl>
2023-10-25 08:24:07 -0300

    Update postmeetings

--- microservice-postmeetings/src/post-meeting/post-meeting.controller.ts
+++ microservice-postmeetings/src/post-meeting/post-meeting.controller.ts
@@ -1,74 +1,75 @@
 
 import { Controller } from '@nestjs/common';
-import {
-    Body,
-    Delete,
-    Get,
-    HttpException,
-    HttpStatus,
-    Param,
-    Post,
-    Put,
-  } from '@nestjs/common';
 import { PostMeetingService } from './post-meeting.service';
-import { PostMeetingDTO } from './dto/post-meeting.dto';
 import { PostMeetingMSG } from 'src/common/constants';
 import { MessagePattern, Payload } from '@nestjs/microservices';
 @Controller()
 export class PostMeetingController {
-    constructor(
-        private readonly postMeetingService: PostMeetingService,
-      ) {}
-    
-    /*   @Post()
-      create(@Body() preMeetingDTO: PreMeetingDTO) {
-        return this.preMeetingService.create(preMeetingDTO);
-      } */
-    
-      @MessagePattern(PostMeetingMSG.CREATE)
-      async create(@Payload() meetingId: string
-   
-      ) {
-          return this.postMeetingService.create(meetingId);
-      }
-    
-      @MessagePattern(PostMeetingMSG.FIND_ALL)
-      findAll() {
-        return this.postMeetingService.findAll();
-      }
-    
-      @MessagePattern(PostMeetingMSG.FIND_ONE)
-      findOne(@Payload() id: string) {
-        return this.postMeetingService.findOne(id);
-      }
-    
-      @MessagePattern(PostMeetingMSG.UPDATE)
-      update(@Payload() payload: any) {
-        return this.postMeetingService.update(payload.id, payload.postMeetingDTO);
-      }
-      
-    
-      @MessagePattern(PostMeetingMSG.DELETE)
-      delete(@Payload() id: string) {
-        return this.postMeetingService.delete(id);
-      }
-    
-    /*   @Post(':preMeetingId/meetingMinute/:meetingMinuteId')
-      async addGuest(
-        @Param('preMeetingId') preMeetingId: string,
-        @Param('meetingMinuteId') meetingMinuteId: string,
-      ) {
-        const meetingMinute = await this.meetingMinuteService.findOne(
-          meetingMinuteId,
-        );
-        if (!meetingMinute) {
-          throw new HttpException('Minuta no encontrada', HttpStatus.NOT_FOUND);
-        } else {
-          return this.preMeetingService.addMeetingMinute(
-            preMeetingId,
-            meetingMinuteId,
-          );
-        }
-      } */
+  constructor(
+    private readonly postMeetingService: PostMeetingService,
+  ) { }
+
+  /* 
+ Modelo estructural de datos:
+
+     1. IPostMeeting:    Interface
+
+     2. PostMeetingMSG:  Mensajeria por RabbitMQ
+
+     3. postMeetingDTO:  PostMeetingDTO: Objeto de transferencia de datos 
+
+ */
+
+  // METODOS CRUD para notificaciones
+
+  /*  
+  Metodo para crear una nueva post reunión.
+  entrada: datos de la post reunión. 
+  salida: objeto de nueva post reunión.  
+  */
+  @MessagePattern(PostMeetingMSG.CREATE)
+  async create(@Payload() meetingId: string
+  ) {
+    return this.postMeetingService.create(meetingId);
+  }
+
+  /*  
+  Método para obtener todas las post reuniones.
+  salida: objeto de post reuniones encontradas. 
+  */
+  @MessagePattern(PostMeetingMSG.FIND_ALL)
+  findAll() {
+    return this.postMeetingService.findAll();
+  }
+
+  /*  
+  Método para  obtener una post reunión a partir del id.
+  entrada: id de la post reunión. 
+  salida: objeto de la post reunión encontrada.  
+    */
+  @MessagePattern(PostMeetingMSG.FIND_ONE)
+  findOne(@Payload() id: string) {
+    return this.postMeetingService.findOne(id);
+  }
+
+  /*  
+  Método para actualizar una post reunión a partir del id.
+  entrada: id de la post reunión y nuevos datos de la post reunión. 
+  salida: objeto de la post reunión actualizada.
+  */
+  @MessagePattern(PostMeetingMSG.UPDATE)
+  update(@Payload() payload: any) {
+    return this.postMeetingService.update(payload.id, payload.postMeetingDTO);
+  }
+
+  /*  
+  Método para borrar permanentemente una post reunión a partir del id.
+  entrada: id de la post reunión.
+  salida: valor booleano de confirmación.
+  */
+  @MessagePattern(PostMeetingMSG.DELETE)
+  delete(@Payload() id: string) {
+    return this.postMeetingService.delete(id);
+  }
 
 }
--- microservice-postmeetings/src/post-meeting/post-meeting.service.ts
+++ microservice-postmeetings/src/post-meeting/post-meeting.service.ts
@@ -7,61 +7,63 @@ import { PostMeetingDTO } from './dto/post-meeting.dto';
 
 @Injectable()
 export class PostMeetingService {
+  constructor(
+    @InjectModel(POSTMEETING.name)
+    private readonly model: Model<IPostMeeting>,
+  ) { }
 
-    
-    constructor(
-        @InjectModel(POSTMEETING.name)
-        private readonly model: Model<IPostMeeting>,
-      ) {}
-    
-    /*   async create(preMeetingDTO: PreMeetingDTO): Promise<IPreMeeting> {
-        const newMeetingMinute = new this.model(preMeetingDTO);
-        return await newMeetingMinute.save();
-      } */
-    
-      async create(meetingId: string){
-        const params={
-          meeting: meetingId,
-          meetingMinute: 'acta dialogica'
-        };
-        const newPostMeeting = new this.model(params);
-        return await newPostMeeting.save();
-      }
-    
-      async findAll(): Promise<IPostMeeting[]> {
-        return await this.model.find();
-      }
-    
-      async findOne(id: string): Promise<IPostMeeting> {
-        return await this.model.findById(id);
-      }
-    
-      async update(id: string, postMeetingDTO: PostMeetingDTO): Promise<IPostMeeting> {
-        return await this.model.findByIdAndUpdate(id, postMeetingDTO, {
-          new: true,
-        });
-      }
-    
-      async delete(id: string): Promise<any> {
-        await this.model.findByIdAndDelete(id);
-        return {
-          status: HttpStatus.OK,
-          msg: 'Deleted',
-        };
-      }
-    
-      async addMeetingMinute(
-        inMeetingId: String,
-        meetingMinuteId: string,
-      ): Promise<IPostMeeting> {
-    
-    
-        return await this.model.findByIdAndUpdate(
-          inMeetingId,
-          {
-            $addToSet: { meetingMinutes: meetingMinuteId },
-          },
-          { new: true },
-        );
-      }
+  /*  
+  Metodo para crear una nueva post reunión.
+  entrada: datos de la post reunión. 
+  salida: objeto de nueva post reunión.  
+  */
+  async create(meetingId: string) {
+    const params = {
+      meeting: meetingId,
+      meetingMinute: 'acta dialogica'
+    };
+    const newPostMeeting = new this.model(params);
+    return await newPostMeeting.save();
+  }
+
+  /*  
+  Método para obtener todas las post reuniones.
+  salida: objeto de post reuniones encontradas. 
+  */
+  async findAll(): Promise<IPostMeeting[]> {
+    return await this.model.find();
+  }
+
+  /*  
+  Método para  obtener una post reunión a partir del id.
+  entrada: id de la post reunión. 
+  salida: objeto de la post reunión encontrada.  
+    */
+  async findOne(id: string): Promise<IPostMeeting> {
+    return await this.model.findById(id);
+  }
+
+  /*  
+  Método para actualizar una post reunión a partir del id.
+  entrada: id de la post reunión y nuevos datos de la post reunión. 
+  salida: objeto de la post reunión actualizada.
+  */
+  async update(id: string, postMeetingDTO: PostMeetingDTO): Promise<IPostMeeting> {
+    return await this.model.findByIdAndUpdate(id, postMeetingDTO, {
+      new: true,
+    });
+  }
+
+  /*  
+  Método para borrar permanentemente una post reunión a partir del id.
+  entrada: id de la post reunión.
+  salida: valor booleano de confirmación.
+  */
+  async delete(id: string): Promise<any> {
+    await this.model.findByIdAndDelete(id);
+    return {
+      status: HttpStatus.OK,
+      msg: 'Deleted',
+    };
+  }
 }
